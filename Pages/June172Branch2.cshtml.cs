30f25d273b9ec98f2ca356a5cbcdca6b3783c1df
steven-buettner44 <66680160+steven-buettner44@users.noreply.github.com>
2020-06-17 10:26:19 -0500

    Branch2

---
+++ Pages/June172Branch2.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace June17._2.Pages
+{
+    public class June172Branch2Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
